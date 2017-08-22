ebcdb9b293a7007a54dba6a878ec3d9f105b7847
Tony Rail <tmamacos@gmail.com>
2017-08-22 21:06:44 +0200

    Removed unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './tutuka.png';
 import './App.css';
+import babyparse from 'babyparse';
+import fs from 'fs';
+
+var file = 'test.csv';
+
+function FileInput(props) {
+  return <div><label> {props.description} </label><input type="file" accept='.csv' name={props.name}/></div>;
+}
 
 class App extends Component {
   render() {
@@ -8,11 +16,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Tutuka File Recon</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          To get started, choose two files to upload.
+          </p>
+          <br/><FileInput description="File 1" name="file1"/>
+          <br/><FileInput description="File 2" name="file2"/>
+          <br/><button onclick="activateLasers()">Process Recon</button>
       </div>
     );
   }
