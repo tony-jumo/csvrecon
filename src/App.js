64e8155af23264ba1efc337ae42765ae59bf5316
Tony Rail <tmamacos@gmail.com>
2017-08-28 00:29:16 +0200

    Initial deploy

--- src/App.js
+++ src/App.js
@@ -1,115 +1,106 @@
 import React, { Component } from 'react';
 import logo from './tutuka.png';
 import './App.css';
-import Papa from 'papaparse'
-
-function FileInput(props) {
-  return <div><label> {props.description} </label><input type="file" accept='.csv' name={props.name}/></div>;
-}
-
-function buildHash(dataRow) {
-  var hashString = ""
-  var row = dataRow[0]
-
-  hashString = row.ProfileName + '|' +
-    row.TransactionDate + '|' +
-    row.TransactionAmount +  '|' + 
-    row.TransactionNarrative + '|' + 
-    row.TransactionDescription + '|' + 
-    row.TransactionID + '|' + 
-    row.TransactionType + '|' +
-    row.WalletReference
-
-  var hash = numbersconv(hashString)
-  return hash
-}
-
-function numbersconv(str) {
-  var res = 0,
-      len = str.length;
-  for (var i = 0; i < len; i++) {
-   res = res * 31 + str.charCodeAt(i);
-   res = res & res;
-  }
-  return res;
- }
- 
-function processFile(file,fileHash) {
-  var fileDetails = [];
-  fileDetails['records'] = 0;
-  fileDetails['errors'] = 0;
-  fileDetails['duplicates'] = 0;
-  fileDetails['matches'] = 0;
-
-  Papa.parse(file, {
-      header: true,
-      skipEmptyLines: true,
-      dynamicTyping: true,
-      step: function(results) {
-        // Ignore TooManyFields errors since we can't disable it for the trailing comma.
-        var errors = results.errors.filter(function (error) {
-          return error.code !== "TooManyFields";
-        });
-
-        // Counts as a record in all scenarios
-        fileDetails['records'] = fileDetails['records'] + 1
-
-        if (errors.length === 0) {
-          var hash = buildHash(results.data)
-          if (fileHash.hasOwnProperty(hash)) {
-            //Collision
-            if (fileHash[hash].fileNames.indexOf(file.name) > -1) {
-              //File already exists in array
-              fileDetails['duplicates'] = fileDetails['duplicates'] + 1
-            } else {
-              //It's a match
-              fileDetails['matches'] = fileDetails['matches'] + 1
-              
-              // Update the filename hash to include this file
-              fileHash[hash].fileNames.push(file.name) 
-              
-              //Remove the data, it's no longer neccesary
-              delete fileHash[hash]
-            }
-          }
-          else {
-            fileHash[hash] = { fileNames: [file.name], rowData: results.data[0] }
-          }
-        }
-        else {
-          // Count the error if it's flagged as such
-          fileDetails['errors'] = fileDetails['errors'] + 1;
-        }
-      }
-    });
-
-    return fileDetails;
-}
+import { FileInput, processFile } from "./Utils";
+import FileDetails from './FileDetails';
+import FileRecon from './FileRecon';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file1: null,
+      file2: null,
+      fileDetails1: {name: 'n/a', records:0, duplicates:0, errors:0, matches: 0},
+      fileDetails2: {name: 'n/a', records:0, duplicates:0, errors:0, matches: 0},
+      fileHashArray: [],
+      gridData: []
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Tutuka File Recon</h2>
         </div>
         <p className="App-intro">
-          To get started, choose two files to upload.
-          </p>
-          <br/><FileInput description="File 1" name="file1"/>
-          <br/><FileInput description="File 2" name="file2"/>
-          <br/><button onClick={this.onButtonClick}>Process Recon</button>
+          To get started, choose two files to process.
+        </p>
+        <br /><FileInput description="File 1" name="file1" />
+        <br /><FileInput description="File 2" name="file2" />
+        <br /><button onClick={this.onButtonClick.bind(this)}>Process Recon</button>
+        <br />
+        <br />
+        <div style={{ float: 'left' }}>
+          <FileDetails data={this.state.fileDetails1} />
+        </div>
+        <div style={{ float: 'right' }}>
+          <FileDetails data={this.state.fileDetails2} />
+        </div>
+        <div style={{ clear: 'both' }}>Below</div>
+        <br /><FileRecon data={this.state.gridData} />
       </div>
     );
   }
+  
+  buildGridRow(tag, row) {
+    var rowData = []
+    rowData[tag+"transactionAmount"] = row.TransactionAmount;
+    rowData[tag+"transactionDate"] = row.TransactionDate;
+    rowData[tag+"walletReference"] = row.WalletReference;
+    
+    return rowData;
+  }
+  
+  buildGridData(fileHash) {
+    var gridData = [];
+    for (var key in fileHash) {
+      if (fileHash.hasOwnProperty(key)) {
+        var element = fileHash[key];
+        if (element.fileNames[0] === this.state.fileDetails1.name) {
+          gridData.push(this.buildGridRow('file1',element.rowData));
+        }
+        else {
+          gridData.push(this.buildGridRow('file2',element.rowData));
+        }
+      }
+    }
+    this.setState({gridData: gridData});
+  }
+
+  setFileDetailsData(fileDetails,fileHash) {
+    if (fileDetails.name === this.state.fileDetails1.name) {
+      this.setState({
+        fileDetails1: fileDetails,
+        fileHashArray: fileHash
+      });
+    }
+    else if (fileDetails.name === this.state.fileDetails2.name) {
+      // Keep the matches in sync with the 2nd file
+      var tempFileDetails1 = this.state.fileDetails1;
+      tempFileDetails1.matches = this.state.fileDetails2.matches;
+
+      this.setState({
+        fileDetails1: tempFileDetails1,
+        fileDetails2: fileDetails,
+        fileHashArray: fileHash
+      });
+
+      this.buildGridData(fileHash);      
+    }
+  }
 
   onButtonClick() {
-    var fileHashArray = [];
+    var props = {};
+    props.fileHashArray = [];
+    props.fileDetails1 = processFile(this.setFileDetailsData.bind(this), document.getElementsByName("file1")[0].files[0], props.fileHashArray);
+    props.fileDetails2 = processFile(this.setFileDetailsData.bind(this), document.getElementsByName("file2")[0].files[0], props.fileHashArray);
 
-    var fileDetails1 = processFile(document.getElementsByName("file1")[0].files[0],fileHashArray);
-    var fileDetails2 = processFile(document.getElementsByName("file2")[0].files[0],fileHashArray);
-    fileDetails1['matches'] = fileDetails2['matches']
+    this.setState({
+      fileDetails1: props.fileDetails1,
+      fileDetails2: props.fileDetails2
+    });
   }
 }
 
